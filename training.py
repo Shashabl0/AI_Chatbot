9ab2b820ef55740d0e57a4f0ecca0f779af31bb6
Shashank Barnwal <shashank.barnwal1999@gmail.com>
2021-08-05 21:46:31 +0530

    commit

--- training.py
+++ training.py
@@ -25,7 +25,7 @@ ignore_letters = ['?',"!",".",",",";)",":)"]
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
-        words.extend(word_list);
+        words.extend(word_list)
         documents.append((word_list,intent['tag']))
 
         if intent['tag'] not in classes:
